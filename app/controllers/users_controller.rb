2ef764f58c33cfb8e1fab026058168a6b18f9589
Alexis Montealegre <ajmu26@gmail.com>
2020-09-28 17:08:27 -0300

    almost done

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+end
+def show
+end
+
+def dashboard
+    @users = User.all
+end
+
+def make_admin
+    user = User.find params[:id]
+    user.update( :admin => true )
+    redirect_to dashboard_users_path(current_user)
+end
+
+def delete_admin
+    user = User.find params[:id]
+    user.update( :admin => nil )
+    redirect_to dashboard_users_path(current_user)
+end
+
+
+
+end
